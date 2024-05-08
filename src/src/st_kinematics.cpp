2aa6f5b5291b2b40e698be9172e30b4d8fcec42a
kms8527 <kim_min_sung@naver.com>
2024-05-08 21:19:59 +0900

    update simulator model

--- src/src/st_kinematics.cpp
+++ src/src/st_kinematics.cpp
@@ -10,8 +10,7 @@ using namespace racecar_simulator;
 // Vehicle Models
 // https://gitlab.lrz.de/tum-cps/commonroad-vehicle-models/blob/master/vehicleModels_commonRoad.pdf
 
-CarState STKinematics::update(const CarState start, double accel,
-                              double steer_angle_vel, CarParams p, double dt) {
+CarState STKinematics::update(const CarState start, double accel, double steer_angle_vel, CarParams p, double dt) {
 
     double thresh = 1.03; // cut off to avoid singular behavior
     double err = .03;     // deadband to avoid flip flop
@@ -49,19 +48,12 @@ CarState STKinematics::update(const CarState start, double accel,
 
     double theta_double_dot =
         (p.friction_coeff * p.mass / (p.I_z * p.wheelbase)) *
-        (p.l_f * p.cs_f * start.steer_angle * (rear_val) +
-         start.slip_angle *
-             (p.l_r * p.cs_r * (front_val)-p.l_f * p.cs_f * (rear_val)) -
-         vel_ratio * (std::pow(p.l_f, 2) * p.cs_f * (rear_val) +
-                      std::pow(p.l_r, 2) * p.cs_r * (front_val)));
+        (p.l_f * p.cs_f * start.steer_angle * (rear_val) + start.slip_angle * (p.l_r * p.cs_r * (front_val)-p.l_f * p.cs_f * (rear_val)) -
+         vel_ratio * (std::pow(p.l_f, 2) * p.cs_f * (rear_val) + std::pow(p.l_r, 2) * p.cs_r * (front_val)));
 
-    double slip_angle_dot =
-        (first_term) *
-            (p.cs_f * start.steer_angle * (rear_val)-start.slip_angle *
-                 (p.cs_r * (front_val) + p.cs_f * (rear_val)) +
-             vel_ratio *
-                 (p.cs_r * p.l_r * (front_val)-p.cs_f * p.l_f * (rear_val))) -
-        start.angular_velocity;
+    double slip_angle_dot = (first_term) * (p.cs_f * start.steer_angle * (rear_val)-start.slip_angle * (p.cs_r * (front_val) + p.cs_f * (rear_val)) +
+                                            vel_ratio * (p.cs_r * p.l_r * (front_val)-p.cs_f * p.l_f * (rear_val))) -
+                            start.angular_velocity;
 
     // update state
     end.x = start.x + x_dot * dt;
@@ -80,9 +72,7 @@ CarState STKinematics::update(const CarState start, double accel,
     return end;
 }
 
-CarState STKinematics::update_k(const CarState start, double accel,
-                                double steer_angle_vel, CarParams p,
-                                double dt) {
+CarState STKinematics::update_k(const CarState start, double accel, double steer_angle_vel, CarParams p, double dt) {
 
     CarState end;
 
@@ -91,12 +81,9 @@ CarState STKinematics::update_k(const CarState start, double accel,
     double y_dot = start.velocity * std::sin(start.theta);
     double v_dot = accel;
     double steer_angle_dot = steer_angle_vel;
-    double theta_dot =
-        start.velocity / p.wheelbase * std::tan(start.steer_angle);
-    double theta_double_dot =
-        accel / p.wheelbase * std::tan(start.steer_angle) +
-        start.velocity * steer_angle_vel /
-            (p.wheelbase * std::pow(std::cos(start.steer_angle), 2));
+    double theta_dot = start.velocity / p.wheelbase * std::tan(start.steer_angle);
+    double theta_double_dot = accel / p.wheelbase * std::tan(start.steer_angle) +
+                              start.velocity * steer_angle_vel / (p.wheelbase * std::pow(std::cos(start.steer_angle), 2));
     double slip_angle_dot = 0;
 
     // update state
@@ -116,9 +103,7 @@ CarState STKinematics::update_k(const CarState start, double accel,
     return end;
 }
 
-CarState STKinematics::update_with_pacejka(const CarState start, double accel,
-                                           double steer_angle_vel,
-                                           const CarParams &p, double dt) {
+CarState STKinematics::update_with_pacejka(const CarState start, double accel, double steer_angle_vel, const CarParams &p, double dt) {
     double thresh = 1.03; // cut off to avoid singular behavior
     double err = .03;     // deadband to avoid flip flop
 
@@ -141,7 +126,7 @@ CarState STKinematics::update_with_pacejka(const CarState start, double accel,
 
     // F_fy와 F_ry 계산
     double F_fy = getForceFront(start, p); // 전륜 측면력
-    double F_ry = getForceRear(start, p); // 후륜 측면력
+    double F_ry = getForceRear(start, p);  // 후륜 측면력
 
     // theta_double_dot과 slip_angle_dot 계산
     double theta_double_dot = (p.l_f * F_fy * std::cos(start.steer_angle) - p.l_r * F_ry) / p.I_z;
@@ -167,35 +152,35 @@ CarState STKinematics::update_with_pacejka(const CarState start, double accel,
 double STKinematics::getForceFront(const CarState &state, const CarParams &p) {
     const double B = 20.0; // 스티프니스 계수
     const double C = 1.9;  // 모양 계수
-    const double D = 20.0;  // 최대 힘
+    const double D = 20.0; // 최대 힘
 
     // 슬립 각도를 라디안으로 변환
     double alpha = getFrontSlipAngle(state, p);
 
     // D * sin(C * arctan(B * alpha)) 형태로 측면 힘을 계산
-    return - D * std::sin(C * std::atan(B * alpha));
+    return D * std::sin(C * std::atan(B * alpha));
 }
 
 double STKinematics::getForceRear(const CarState &state, const CarParams &p) {
     const double B = 20.0; // 스티프니스 계수
     const double C = 1.9;  // 모양 계수
-    const double D = 20.0;  // 최대 힘
+    const double D = 20.0; // 최대 힘
 
     // 슬립 각도를 라디안으로 변환
     double alpha = getRearSlipAngle(state, p);
-    return - D * std::sin(C * std::atan(B * alpha));
+    return D * std::sin(C * std::atan(B * alpha));
 }
 
 double STKinematics::getFrontSlipAngle(const CarState &state, const CarParams &p) {
     double vx = state.velocity * std::cos(state.slip_angle);
     double vy = state.velocity * std::sin(state.slip_angle);
-    double front_slip_angle = std::atan2(vy + p.l_f * state.angular_velocity, vx) - state.steer_angle;
+    double front_slip_angle = -std::atan2(vy + p.l_f * state.angular_velocity, vx) + state.steer_angle;
     return front_slip_angle;
 }
 
 double STKinematics::getRearSlipAngle(const CarState &state, const CarParams &p) {
     double vx = state.velocity * std::cos(state.slip_angle);
     double vy = state.velocity * std::sin(state.slip_angle);
-    double rear_slip_angle = std::atan2(vy - p.l_r * state.angular_velocity, vx);
+    double rear_slip_angle = -std::atan2(vy - p.l_r * state.angular_velocity, vx);
     return rear_slip_angle;
 }
